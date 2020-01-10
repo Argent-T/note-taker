725a0408b919507eaa16c67b4ad52c630b1390b2
Thomas Argent <tfargent@gmail.com>
2020-01-10 15:42:45 -0500

    fixed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -66,11 +66,8 @@ var handleNoteSave = function() {
 var handleNoteDelete = function(event) {
   // prevents the click listener for the list from being called when the button inside of it is clicked
   event.stopPropagation();
-
-  var note = $(this)
-    .parent(".list-group-item")
-    .data();
-
+  var note = $(this).parent(".list-group-item").data();
+ 
   if (activeNote.id === note.id) {
     activeNote = {};
   }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ==============================================================================
 
 var express = require("express");
-
+const fs = require("fs");
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -14,17 +14,19 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(__dirname + '/public'));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+
+
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
