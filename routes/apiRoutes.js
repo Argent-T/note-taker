cd0a8abd27078674d11ab2429b8a70cee8bf9b49
Thomas Argent <tfargent@gmail.com>
2020-01-14 18:14:42 -0500

    readme

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,6 +19,8 @@ module.exports = function(app) {
 
         res.redirect('back');
     });
+
+    
     app.delete("/api/notes/:id", function (req,res) {
         console.log(req.params.id);
         notesdata.splice((req.params.id -1),1);
